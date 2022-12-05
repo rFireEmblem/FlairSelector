85151456df7db72233a5ddea11bbd0033dd41efa
toffee7756 <aiman3977@gmail.com>
2022-12-05 13:28:51 -0800

    half themes done

--- scripts.js
+++ scripts.js
@@ -9,10 +9,12 @@
 
 */
 const NUMBER_OF_ARRAYS = 42
-const games = ['threehouses', 'archanea', 'sov', 'genealogy', 'thracia', 'seals', 'sword', 'stones', 'por', 'rd', 'awakening', 'fates', 'feh', 'tms', 'warriors']
+const games = ['threehouses', 'archanea', 'sov', 'genealogy', 'thracia', 'seals', 'sword', 'stones', 'por', 'rd', 'awakening', 'fates', 'feh', 'tms', 'warriors', 'cipher', 'engage']
+const buttons = ['archanea', 'sov', 'genealogy', 'thracia', 'seals', 'sword', 'stones', 'por', 'rd', 'awakening', 'fates', 'threehouses', 'engage', 'feh', 'tms', 'warriors', 'cipher']
+//const themes = ['archanea', 'sov', 'genealogy', 'thracia', 'seals', 'sword', 'stones', 'por', 'rd', 'awakening', 'fates', 'threehouses','engage', 'feh', 'tms']
+const themes = ['sov', 'stones', 'awakening', 'fates', 'threehouses', 'feh', ]
 let current = 'threehouses';
 //cipher
-
 let arrays = {};
 let selected = [];
 arrkeys = []
@@ -23,6 +25,25 @@ function clearSelects() {
   let tempstr = "<button id='clear' onclick='clearSelects()'><img src=\"./clear.png\"></button>"
   tempstr += `<button id='go' onclick='request()'> <img src="./go.png"> </button>`
   document.getElementById("portraitBox").innerHTML = tempstr
+}
+
+function generateButtonBar() {
+  let element = document.getElementById('buttonBox');
+  let str = " "
+  for (let j = 0; j < buttons.length; j++) {
+    str += `<button onclick="filter('${buttons[j]}')">`
+    for (let i = 0; i < themes.length; i++) {
+      str += `<img class='${themes[i]}' src="title_cards/${themes[i]}/${buttons[j]}.png" style="display:none;"`
+      if (themes[i] == buttons[j] || (themes[i] == 'sov' && buttons[j] == 'cipher') || (themes[i] == 'awakening' && buttons[j] == 'warriors')) {
+        str += `id="${buttons[j]}img">`
+      }
+      else {
+        str += '>'
+      }
+    }
+    str += `</button>`
+  }
+  element.innerHTML = str;
 }
 
 function addToBar(flaircode) {
@@ -41,41 +62,35 @@ function addToBar(flaircode) {
   }
 }
 
-function filter(game){
-
+function filter(game) {
   console.log('test')
-
   let nodes = document.getElementsByClassName(current)
-    for (let i = 0; i < nodes.length; i++) {
-      let hero = nodes[i];
-      hero.style.display = 'none';
-    }
-
-   
- nodes = document.getElementsByClassName(game)
- console.log(nodes)
-    for (let i = 0; i < nodes.length; i++) {
-      let hero = nodes[i];
-      hero.style.display = 'inline-block';
-    }
-
+  console.log(nodes)
+  for (let i = 0; i < nodes.length; i++) {
+    let hero = nodes[i];
+    hero.style.display = 'none';
+  }
+  nodes = document.getElementsByClassName(game)
+  console.log(nodes)
+  for (let i = 0; i < nodes.length; i++) {
+    let hero = nodes[i];
+    hero.style.display = 'inline-block';
+  }
   let id = `${current}img`;
-  let file = `title_cards/${current}.png`;
+  let file = `title_cards/${current}/${current}.png`;
   document.getElementById(id).src = file;
 
   id = `${game}img`;
-  file = `title_cards/${game}_sel.png`;
+  file = `title_cards/${game}/${game}_sel.png`;
   document.getElementById(id).src = file;
   current = game;
-
+  document.getElementsByTagName("body")[0].style.backgroundImage = `url(assets/backdrops/${game}.png)`
 }
-
 async function generate() {
   let tempStr = ''
   for (let i = 0; i < games.length; i++) {
     tempStr += `<div class="${games[i]}" style="display:none;"><br>`
     let entry = data[games[i]]
-   
     let selects = entry.sections;
     for (let j = 0; j < selects.length; j++) {
       tempStr += `<br><br>${selects[j]}<br><br>`
@@ -88,27 +103,23 @@ async function generate() {
     tempStr += "</div>"
   }
   document.getElementById("main").innerHTML = tempStr
- 
   let nodes = document.getElementsByClassName(`threehouses`)
- 
-    for (let i = 0; i < nodes.length; i++) {
-     let hero = nodes[i];
- 
-      hero.style.display = 'inline-block';
-    }
-    
+  for (let i = 0; i < nodes.length; i++) {
+    let hero = nodes[i];
+    hero.style.display = 'inline-block';
+  }
+  id = `threehousesimg`;
+  file = `title_cards/threehouses/threehouses_sel.png`;
+  document.getElementById(id).src = file;
+
 }
 
-function request(){
+function request() {
   let url = `https://www.reddit.com/message/compose/?to=Bot-ta_The_Beast&subject=flair&message=`
   for (let i = 0; i < selected.length; i++) {
     url += selected[i]
     url += '%0a'
   }
   url = url.slice(0, -3)
-
-  window.open(url,'_blank');
-
-
+  window.open(url, '_blank');
 }
-
